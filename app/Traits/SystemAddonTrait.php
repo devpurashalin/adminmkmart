b05ff0791bdad0dbab516ac8e9c63770948147da
Shalin Devpura <shalindevpura19@gmail.com>
2024-08-05 09:15:39 +0000

    chanage

--- app/Traits/SystemAddonTrait.php
+++ app/Traits/SystemAddonTrait.php
@@ -9,50 +9,58 @@ trait SystemAddonTrait
      */
     public function get_addons(): array
     {
-        $dir = 'Modules';
+        $dir = base_path('Modules');
         $directories = self::getDirectories($dir);
 
         $addons = [];
         foreach ($directories as $directory) {
-            $sub_dirs = self::getDirectories('Modules/' . $directory);
+            $sub_dirs = self::getDirectories($dir . '/' . $directory);
             if (in_array('Addon', $sub_dirs)) {
-                $addons[] = 'Modules/' . $directory;
+                $addons[] = $dir . '/' . $directory;
             }
         }
 
         $array = [];
         foreach ($addons as $item) {
-            $full_data = include($item . '/Addon/info.php');
-            $array[] = [
-                'addon_name' => $full_data['name'],
-                'software_id' => $full_data['software_id'],
-                'is_published' => $full_data['is_published'],
-            ];
+            $infoFile = $item . '/Addon/info.php';
+            if (file_exists($infoFile)) {
+                $full_data = include($infoFile);
+                $array[] = [
+                    'addon_name' => $full_data['name'],
+                    'software_id' => $full_data['software_id'],
+                    'is_published' => $full_data['is_published'],
+                ];
+            }
         }
 
         return $array;
     }
 
     /**
-     * @return array
      */
     public function get_addon_admin_routes(): array
     {
-        $dir = 'Modules';
+        $dir = base_path('Modules');
         $directories = self::getDirectories($dir);
         $addons = [];
         foreach ($directories as $directory) {
-            $sub_dirs = self::getDirectories('Modules/' . $directory);
+            $sub_dirs = self::getDirectories($dir . '/' . $directory);
             if (in_array('Addon', $sub_dirs)) {
-                $addons[] = 'Modules/' . $directory;
+                $addons[] = $dir . '/' . $directory;
             }
         }
 
         $full_data = [];
         foreach ($addons as $item) {
-            $info = include($item . '/Addon/info.php');
-            if ($info['is_published']){
-                $full_data[] = include($item . '/Addon/admin_routes.php');
+            $infoFile = $item . '/Addon/info.php';
+            if (file_exists($infoFile)) {
+                $info = include($infoFile);
+                if ($info['is_published']) {
+                    $adminRoutesFile = $item . '/Addon/admin_routes.php';
+                    if (file_exists($adminRoutesFile)) {
+                        $full_data[] = include($adminRoutesFile);
+                    }
+                }
             }
         }
 
@@ -64,31 +72,29 @@ trait SystemAddonTrait
      */
     public function get_payment_publish_status(): array
     {
-        $dir = 'Modules'; // Update the directory path to Modules/Gateways
+        $dir = base_path('Modules'); // Adjust the directory path to Modules/Gateways if necessary
         $directories = self::getDirectories($dir);
-        // dd($directories);
         $addons = [];
         foreach ($directories as $directory) {
-            $sub_dirs = self::getDirectories($dir . '/' . $directory); // Use $dir instead of 'Modules/'
-            if($directory == 'Gateways'){
-                if (in_array('Addon', $sub_dirs)) {
-                    $addons[] = $dir . '/' . $directory; // Use $dir instead of 'Modules/'
-                }
+            $sub_dirs = self::getDirectories($dir . '/' . $directory);
+            if ($directory == 'Gateways' && in_array('Addon', $sub_dirs)) {
+                $addons[] = $dir . '/' . $directory;
             }
         }
 
         $array = [];
         foreach ($addons as $item) {
-            $full_data = include($item . '/Addon/info.php');
-            $array[] = [
-                'is_published' => $full_data['is_published'],
-            ];
+            $infoFile = $item . '/Addon/info.php';
+            if (file_exists($infoFile)) {
+                $full_data = include($infoFile);
+                $array[] = [
+                    'is_published' => $full_data['is_published'],
+                ];
+            }
         }
-
 
         return $array;
     }
-
 
     /**
      * @param string $path
@@ -96,6 +102,10 @@ trait SystemAddonTrait
      */
     function getDirectories(string $path): array
     {
+        if (!is_dir($path)) {
+            return [];
+        }
+
         $directories = [];
         $items = scandir($path);
         foreach ($items as $item) {
